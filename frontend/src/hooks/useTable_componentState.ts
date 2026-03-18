d2d740b341d122d4bf2516cc90e561cf72465371
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-03-18 15:36:42 +0100

    Add data table component - add state hook

---
+++ frontend/src/hooks/useTable_componentState.ts
@@ -0,0 +1,9 @@
+"use client";
+import { useState, useCallback } from 'react';
+
+export function useTable_componentState<T>(initial: T) {
+  const [value, setValue] = useState<T>(initial);
+  const update = useCallback((next: T) => setValue(next), []);
+  const reset = useCallback(() => setValue(initial), [initial]);
+  return { value, update, reset };
+}
