0969e4a64e63c72c1c925d50766d5fbd9dda5ed5
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-03-21 12:38:48 +0100

    Add notification system - commit 39/50

--- frontend/src/types/notification_system.ts
+++ frontend/src/types/notification_system.ts
@@ -4,3 +4,5 @@ export interface Notification_systemD8 { id: string; name: string; value: number
 export interface Notification_systemD18 { id: string; name: string; value: number; ts: number; }
 
 export interface Notification_systemD28 { id: string; name: string; value: number; ts: number; }
+
+export interface Notification_systemD38 { id: string; name: string; value: number; ts: number; }
