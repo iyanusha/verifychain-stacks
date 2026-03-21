6955a92771bac006571c5934067a2bbe82e2dd89
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-03-21 15:18:28 +0100

    Add admin control panel - commit 19/50

--- frontend/src/types/admin_controls.ts
+++ frontend/src/types/admin_controls.ts
@@ -1,2 +1,4 @@
 
 export interface Admin_controlsD8 { id: string; name: string; value: number; ts: number; }
+
+export interface Admin_controlsD18 { id: string; name: string; value: number; ts: number; }
